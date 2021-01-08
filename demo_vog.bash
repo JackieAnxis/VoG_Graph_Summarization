e07c1217fa74a48f3d868a17b4a8850711b21cb4
JackieAnxis <jackieanxis@gmail.com>
2021-01-08 21:47:49 +0800

    fix encoding_top10 and python2.7

--- demo_vog.bash
+++ demo_vog.bash
@@ -2,14 +2,15 @@
 
 echo ''
 echo -e "\e[34m======== Steps 1 & 2: Subgraph Generation and Labeling  ==========\e[0m"
-# matlab -r run_structureDiscovery
+matlab -r run_structureDiscovery
 echo ''
 echo 'Structure discovery finished.'
 
-unweighted_graph='DATA/cliqueStarClique.out'
-model='DATA/cliqueStarClique_orderedALL.model'
-modelFile='cliqueStarClique_orderedALL.model'
-modelTop10='DATA/cliqueStarClique_top10ordered.model'
+name='cliqueStarClique'
+unweighted_graph="DATA/$name.out"
+model="DATA/$name_orderedALL.model"
+modelFile="$name_orderedALL.model"
+modelTop10="DATA/$name_top10ordered.model"
 
 echo ''
 echo -e "\e[34m=============== Step 3: Summary Assembly ===============\e[0m"
@@ -18,7 +19,7 @@ echo -e "\e[31m=============== TOP 10 structures ===============\e[0m"
 head -n 10 $model > $modelTop10
 echo 'Computing the encoding cost...'
 echo ''
-python MDL/score.py $unweighted_graph $modelTop10 
+python MDL/score.py $unweighted_graph $modelTop10  > DATA/encoding_top10.out
 
 echo ''
 echo 'Explanation of the above output:'
@@ -35,7 +36,7 @@ echo ''
 echo -e "\e[31m========= Greedy selection of structures =========\e[0m"
 echo 'Computing the encoding cost...'
 echo ''
-python2.7 MDL/greedySearch_nStop.py $unweighted_graph $model >/dev/null 2>&1
+python MDL/greedySearch_nStop.py $unweighted_graph $model >/dev/null 2>&1
 mv heuristic* DATA/
 echo '>> Outputs saved in DATA/. To interpret the structures that are selected, check the file MDL/readme.txt.'
 echo ": DATA/heuristicSelection_nStop_ALL_$modelFile has the lines of the $model structures included in the summary."
